0a0490754b64e2def27a525854fe9b3f75452c4d
KimSeungTae <iufan1526@gmail.com>
2023-03-04 15:22:01 +0900

    Data Type

--- SwiftStudyPlayGround.playground/Contents.swift
+++ SwiftStudyPlayGround.playground/Contents.swift
@@ -1,4 +1,25 @@
 import UIKit
 
-var greeting = "Hello, playground"
-dasda
+// 자료형
+
+//정수형 : 음수와 양수를 표현
+Int.max
+Int.min
+
+Int8.max
+Int8.min
+
+Int16.max
+Int16.min
+
+Int32.max
+Int32.min
+
+Int64.max
+Int64.min
+
+//부호없는정수 : 양수만을 표현 일반 정수형보다 크기가 두배가 크다
+UInt.max
+UInt.min
+
+//
