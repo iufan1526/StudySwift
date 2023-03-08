635c5e5561fba80a310f10cd8e5682d530596c57
KimSeungTae <iufan1526@gmail.com>
2023-03-08 21:56:47 +0900

    Function Study Update

--- Function.playground/Contents.swift
+++ Function.playground/Contents.swift
@@ -1,3 +1,24 @@
 import UIKit
 
-var greeting = "Hello, playground"
+//함수 정의법
+
+func printHello() {
+    print("안녕하세요!")
+}
+
+func sayHello() -> String {
+    let str = "안녕하세요"
+    return str
+}
+
+func printHelloWithName(name : String) {
+    print("이름은 \(name)")
+}
+
+func sayHelloWithName(name : String) -> String {
+    let result = name
+    return result
+}
+
+printHello()
+printHelloWithName(name : "김승태")
